207dcb9402ed7e2b82f981d5328612a662fdb98c
Roman Pushkov <pushkovroman@me.com>
2018-03-07 19:46:15 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Awesome.Mixfile do
   def project do
     [
       app: :awesome,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
