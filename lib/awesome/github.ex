fa55026c8ea733d8272b39f3a075d05f286595ca
Roman Pushkov <pushkovroman@me.com>
2018-03-21 12:21:22 +0300

    rewrote github module

--- lib/awesome/github.ex
+++ lib/awesome/github.ex
@@ -1,34 +1,46 @@
 defmodule Awesome.Github do
-  import Poison.Parser
-  @test_link "https://api.github.com/repos/h4cc/awesome-elixir?access_token="
+  @moduledoc """
+    Github interaction
+  """
 
-  def get(url) do
-    url
+  import Poison.Parser
+  @list_location "https://raw.githubusercontent.com/h4cc/awesome-elixir/master/README.md"
+
+  @github_repo_api "https://api.github.com/repos"
+  @github_token_query "?access_token="
+
+  @rate "X-RateLimit-Remaining"
+
+  def get_list, do: @list_location |> HTTPoison.get |> handle_response
+
+  def get_repo_data(path) do
+    @github_repo_api <> path <> @github_token_query <> get_token()
+    |> HTTPoison.get
+    |> handle_response
+    |> parse_response
+  end
+
+  defp get_token, do: Application.get_env(:awesome, :github_access_token)
+
+  defp handle_response({:ok, %{body: body, status_code: 200}}), do: {:ok, body}
+  defp handle_response({:ok, %{body: json, status_code: 301}}), do: redirect(parse(json))
+  defp handle_response({:ok, %{headers: headers, status_code: 403}}) do
+    case headers |> Map.new |> Map.fetch(@rate) do
+      {:ok, "0"} ->
+        {:error, :rate_limited}
+      _ ->
+        {:error, :unavailable}
+    end
+  end
+  defp handle_response(_), do: {:error, :unavailable}
+
+  defp redirect({:ok, %{"url" => redirect_url}}) do
+    redirect_url
     |> HTTPoison.get
     |> handle_response
   end
+  defp redirect(_), do: {:error, :unavailable}
 
-  def rate_limited? do
-    1500 > get_rate()
-  end
-
-  def get_token, do: Application.get_env(:awesome, :github_access_token)
-
-  defp handle_response({:ok, %{body: body, status_code: 200}}), do: {:ok, body}
-  defp handle_response({:ok, %{body: redirect_json, status_code: 301}}) do
-    {:ok, %{"url" => redirect_url}} = parse(redirect_json)
-    handle_response(HTTPoison.get(redirect_url))
-  end
-  defp handle_response(_), do: {:error, nil}
-
-  defp get_rate() do
-    @test_link <> get_token()
-    |> HTTPoison.get!
-    |> Map.fetch!(:headers)
-    |> Enum.filter(&(&1 |> elem(0) == "X-RateLimit-Remaining"))
-    |> List.first
-    |> elem(1)
-    |> Integer.parse
-    |> elem(0)
-  end
+  defp parse_response({:ok, body}), do: parse(body)
+  defp parse_response(error),       do: error
 end
