b1f2da6981426d619f3a0d75b9c405a75c4ae3d0
Roman Pushkov <pushkovroman@me.com>
2018-03-21 12:24:01 +0300

    added repo data fetcher

--- lib/awesome/list/storage.ex
+++ lib/awesome/list/storage.ex
@@ -1,20 +1,15 @@
 defmodule Awesome.List.Storage do
+  @moduledoc """
+    Storage management
+  """
   @storage Application.get_env(:awesome, :storage)
   @day_in_seconds 24 * 60 * 60
 
   def get_list do
     {:ok, table} = :dets.open_file(@storage, [type: :set])
-    list = :dets.match_object(table, {:"$1", :"$2", :"$3"})
-    |> Enum.sort
+    list = :dets.match_object(table, {:"$1", {:"$2", :"$3"}})
     :dets.close(table)
-    list
-  end
-
-  def write_list(list) do
-    {:ok, table} = :dets.open_file(@storage, [type: :set])
-    :dets.insert(table, {:latest_update, DateTime.utc_now})
-    list |> Enum.each(&(:dets.insert(table, &1)))
-    :dets.close(table)
+    Enum.sort(list)
   end
 
   def up_to_date? do
@@ -29,6 +24,31 @@ defmodule Awesome.List.Storage do
         time_elapsed(date) < @day_in_seconds
     end
   end
+
+  def get_repo_data(section_name, repo_name) do
+    {:ok, table} = :dets.open_file(@storage, [type: :set])
+    section = :dets.lookup(table, section_name)
+    :dets.close(table)
+
+    find_repo(section, repo_name)
+  end
+
+  def write_list(list) do
+    {:ok, table} = :dets.open_file(@storage, [type: :set])
+    :dets.insert(table, {:latest_update, DateTime.utc_now})
+    Enum.each(list, &(:dets.insert(table, &1)))
+    :dets.close(table)
+  end
+
+  defp find_repo([{_name, {_desc, repos}}], repo_name) do
+    repos
+    |> Map.new
+    |> Map.fetch(repo_name)
+    |> find_repo
+  end
+  defp find_repo(_, _), do: :unavailable
+  defp find_repo({:ok, data}), do: data
+  defp find_repo(:error), do: :unavailable
 
   defp time_elapsed(date), do: DateTime.diff(DateTime.utc_now, date)
 end
