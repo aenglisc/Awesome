b1a881d82b4271f0cb0634472f3d7d6a5b3d819d
Roman Pushkov <pushkovroman@me.com>
2018-03-21 12:23:17 +0300

    rewrote parser

--- lib/awesome/list/parser.ex
+++ lib/awesome/list/parser.ex
@@ -1,85 +1,96 @@
 defmodule Awesome.List.Parser do
+  @moduledoc """
+    List parser
+  """
+
   alias Awesome.Github
-  
+  alias Awesome.List.Storage
+
   @regex_github_link ~r/https:\/\/github.com\/[\w\-]+\/[\w\-]+/
   @regex_repo_name ~r/\[(.*?)\]/
   @regex_description ~r/\)\ \-\ (.+)/
-  @regex_link ~r/\[([^]]*)\]\(([^\s^\)]*)[\s\)]/
-  @github_repo_api "https://api.github.com/repos"
-  @github_token_query "?access_token="
 
-  def parse(data) do
-    data
-    |> remove_resources
-    |> remove_index_and_split_into_sections
+  @regex_link ~r/\[([^]]*)\]\(([^\s^\)]*)[\s\)]/
+  @link_replacement "<a href=\"\\2\">\\1</a>"
+
+  @url "html_url"
+  @stars "stargazers_count"
+  @updated "pushed_at"
+
+  def parse_list(raw_list_string) do
+    raw_list_string
+    |> clean_and_split_into_sections
     |> parse_sections
   end
 
-  defp remove_resources(string) do
+  defp clean_and_split_into_sections(string) do
     string
     |> String.split("\n# ", trim: true)
-    |> Enum.at(0)
-  end
-
-  defp remove_index_and_split_into_sections(string) do
-    string
+    |> List.first
     |> String.split("## ", trim: true)
     |> Enum.slice(1..-1)
   end
 
   defp parse_sections(list) do
     list
-    |> Task.async_stream(&(&1 |> String.split("\n", trim: true) |> build_section_node), timeout: :infinity)
+    |> Task.async_stream(&(build_section_node(&1)), timeout: :infinity)
+    |> Enum.filter(&(match?({:ok, _res}, &1)))
     |> Enum.map(fn {:ok, res} -> res end)
   end
 
-  defp build_section_node([name, description | repos]) do
-    {
-      name,
-      description |> String.slice(1..-2) |> parse_links_in_description,
-      repos |> parse_repos
-    }
+  defp build_section_node(section_string) do
+    [name, description | repos] = String.split(section_string, "\n", trim: true)
+    {name, {parse_section_description(description), parse_repos(repos, name)}}
   end
 
-  defp parse_repos(list) do
+  defp parse_section_description(string) do
+    string
+    |> String.slice(1..-2)
+    |> parse_links_in_description
+  end
+
+  defp parse_repos(list, section_name) do
     list
     |> Enum.filter(&(&1 =~ @regex_github_link))
-    |> Task.async_stream(&(&1 |> build_repo_node), timeout: :infinity)
-    |> Enum.reduce([], fn {:ok, res}, acc -> if res, do: [res | acc], else: acc end)
-    |> Enum.reverse
+    |> Task.async_stream(&(build_repo_node(&1, section_name)), timeout: :infinity)
+    |> Enum.filter(&(match?({:ok, _res}, &1)))
+    |> Enum.map(fn {:ok, res} -> res end)
+    |> Enum.reject(&(match?({_name, :unavailable}, &1)))
   end
 
-  defp build_repo_node(string) do
-    path = string
-    |> get_github_url
-    |> URI.parse
-    |> Map.get(:path)
-
-    result = @github_repo_api <> path <> @github_token_query <> Github.get_token
-    |> Github.get
-
-    case result do
-      {:ok, json} ->
-        {:ok, %{
-          "html_url" => repo_url,
-          "stargazers_count" => stars,
-          "pushed_at" => updated_at
-        }} = Poison.Parser.parse(json)
-        {
-          repo_url,
-          string |> get_repo_name,
-          string |> get_repo_description |> parse_links_in_description,
-          stars,
-          updated_at
-        }
+  defp build_repo_node(repo_string, section_name) do
+    name = get_repo_name(repo_string)
+    case fetch_repo_data(repo_string) do
+      {:ok, %{@url => url, @stars => stars, @updated => updated}} ->
+        {name, {get_repo_description(repo_string), url, stars, updated}}
+      {:error, :rate_limited} ->
+        {name, Storage.get_repo_data(section_name, name)}
       _ ->
-        nil
+        {name, :unavailable}
     end
   end
 
-  defp get_github_url(string),       do: @regex_github_link |> Regex.run(string) |> Enum.at(0)
-  defp get_repo_name(string),        do: @regex_repo_name   |> Regex.run(string) |> Enum.at(1)
-  defp get_repo_description(string), do: @regex_description |> Regex.run(string) |> Enum.at(1)
-  
-  defp parse_links_in_description(string), do: Regex.replace(@regex_link, string, "<a href=\"\\2\">\\1</a>")
+  defp fetch_repo_data(string) do
+    string
+    |> get_repo_path
+    |> Github.get_repo_data
+  end
+
+  defp get_repo_path(string) do
+    string
+    |> get_github_url
+    |> URI.parse
+    |> Map.get(:path)
+  end
+
+  defp get_github_url(string), do: @regex_github_link |> Regex.run(string) |> Enum.at(0)
+  defp get_repo_name(string),  do: @regex_repo_name   |> Regex.run(string) |> Enum.at(1)
+  defp get_repo_description(string) do
+    @regex_description
+    |> Regex.run(string)
+    |> Enum.at(1)
+    |> parse_links_in_description
+  end
+
+  defp parse_links_in_description(string), do: Regex.replace(@regex_link, string, @link_replacement)
 end
