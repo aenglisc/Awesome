663108e6a114f0f8e07accda07415db87e4ff0af
Roman Pushkov <pushkovroman@me.com>
2018-03-24 19:06:06 +0300

    refactored github module

--- lib/awesome/github.ex
+++ lib/awesome/github.ex
@@ -2,28 +2,29 @@ defmodule Awesome.Github do
   @moduledoc """
     Github interaction
   """
-
   import Poison.Parser
-  @list_location "https://raw.githubusercontent.com/h4cc/awesome-elixir/master/README.md"
-
-  @github_repo_api "https://api.github.com/repos"
   @github_token_query "?access_token="
-
   @rate "X-RateLimit-Remaining"
 
-  def get_list, do: @list_location |> HTTPoison.get |> handle_response
+  defp get_list_url, do: Application.get_env(:awesome, :github_list_location)
+  defp get_endpoint, do: Application.get_env(:awesome, :github_api_endpoint)
+  defp get_token,    do: Application.get_env(:awesome, :github_access_token)
 
-  def get_repo_data(path) do
-    @github_repo_api <> path <> @github_token_query <> get_token()
-    |> HTTPoison.get
+  def get_list do
+    get_list_url()
+    |> HTTPoison.get([], follow_redirect: true)
+    |> handle_response
+  end
+
+  def get_repo_data(%{host: "github.com", path: path}) do
+    get_endpoint() <> path <> @github_token_query <> get_token()
+    |> HTTPoison.get([], follow_redirect: true)
     |> handle_response
     |> parse_response
   end
-
-  defp get_token, do: Application.get_env(:awesome, :github_access_token)
+  def get_repo_data(_), do: {:error, :unavailable}
 
   defp handle_response({:ok, %{body: body, status_code: 200}}), do: {:ok, body}
-  defp handle_response({:ok, %{body: json, status_code: 301}}), do: redirect(parse(json))
   defp handle_response({:ok, %{headers: headers, status_code: 403}}) do
     case headers |> Map.new |> Map.fetch(@rate) do
       {:ok, "0"} ->
@@ -33,13 +34,6 @@ defmodule Awesome.Github do
     end
   end
   defp handle_response(_), do: {:error, :unavailable}
-
-  defp redirect({:ok, %{"url" => redirect_url}}) do
-    redirect_url
-    |> HTTPoison.get
-    |> handle_response
-  end
-  defp redirect(_), do: {:error, :unavailable}
 
   defp parse_response({:ok, body}), do: parse(body)
   defp parse_response(error),       do: error
