2ac5d9249d61b8fea8fe6737a8be8ac1176a3f24
Roman Pushkov <pushkovroman@me.com>
2018-03-25 11:50:45 +0300

    streamlined tests

--- test/awesome/awesome_test.exs
+++ test/awesome/awesome_test.exs
@@ -4,25 +4,19 @@ defmodule Awesome.AwesomeTest do
   alias Awesome.List.{Fetcher, Storage}
   import Plug.Conn
 
-  setup do
-    bypass = Bypass.open
-    Application.put_env(:awesome, :github_api_endpoint, "http://localhost:#{bypass.port}")
-    Application.put_env(:awesome, :github_list_location, "http://localhost:#{bypass.port}")
+  @list File.read!(Path.join(__DIR__, "__fixtures__/list.md"))
 
-    {:ok, bypass: bypass}
-  end
-
-  @repo1_uri URI.parse("https://github.com/hello/world")
-  @repo2_uri URI.parse("https://github.com/hello/there")
-  @repo3_uri URI.parse("https://github.com/howdy/world")
-  @repo4_uri URI.parse("https://github.com/howdy/there")
+  @repo1_uri "/hello/world"
+  @repo2_uri "/hello/there"
+  @repo3_uri "/howdy/world"
+  @repo4_uri "/howdy/there"
 
   @repo1_json File.read!(Path.join(__DIR__, "__fixtures__/repo1.json"))
   @repo2_json File.read!(Path.join(__DIR__, "__fixtures__/repo2.json"))
   @repo3_json File.read!(Path.join(__DIR__, "__fixtures__/repo3.json"))
 
   @rate_limit [{"X-RateLimit-Remaining", "0"}]
-  @list File.read!(Path.join(__DIR__, "__fixtures__/list.md"))
+
   @storage_path Path.join(__DIR__, "../../test_storage")
   @parsed_list [
     {"Hello", {"Hello, good sir!",
@@ -56,6 +50,14 @@ defmodule Awesome.AwesomeTest do
     }
   ]
 
+  setup do
+    bypass = Bypass.open
+    Application.put_env(:awesome, :github_api_endpoint, "http://localhost:#{bypass.port}")
+    Application.put_env(:awesome, :github_list_location, "http://localhost:#{bypass.port}")
+
+    {:ok, bypass: bypass}
+  end
+
   test "list unavailable", %{bypass: bypass} do
     Bypass.expect(bypass,
       &send_resp(&1, 404, ""))
@@ -66,91 +68,60 @@ defmodule Awesome.AwesomeTest do
     File.rm(@storage_path)
   end
 
-  test "run daily", %{bypass: bypass} do
+  test "run fetcher", %{bypass: bypass} do
     Bypass.expect(bypass, "GET", "/",
       &send_resp(&1, 200, @list))
 
-    Bypass.expect(bypass, "GET", @repo1_uri.path,
+    Bypass.expect(bypass, "GET", @repo1_uri,
       &send_resp(&1, 200, @repo1_json))
 
-    Bypass.expect(bypass, "GET", @repo2_uri.path,
+    Bypass.expect(bypass, "GET", @repo2_uri,
       &send_resp(&1, 200, @repo2_json))
 
-    Bypass.expect(bypass, "GET", @repo3_uri.path,
+    Bypass.expect(bypass, "GET", @repo3_uri,
       &send_resp(&1, 200, @repo3_json))
 
-    Bypass.expect(bypass, "GET", @repo4_uri.path,
+    Bypass.expect(bypass, "GET", @repo4_uri,
       &send_resp(&1, 404, ""))
+
+    assert Storage.up_to_date? == false
 
     assert Fetcher.update_list(:daily) == :ok
-    assert Storage.get_list == @parsed_list
-
-    File.rm(@storage_path)
-  end
-
-  test "run on reboot", %{bypass: bypass} do
-    Bypass.expect(bypass, "GET", "/",
-      &send_resp(&1, 200, @list))
-
-    Bypass.expect(bypass, "GET", @repo1_uri.path,
-      &send_resp(&1, 200, @repo1_json))
-
-    Bypass.expect(bypass, "GET", @repo2_uri.path,
-      &send_resp(&1, 200, @repo2_json))
-
-    Bypass.expect(bypass, "GET", @repo3_uri.path,
-      &send_resp(&1, 200, @repo3_json))
-
-    Bypass.expect(bypass, "GET", @repo4_uri.path,
-      &send_resp(&1, 404, ""))
-
-    assert Fetcher.update_list(:reboot) == :ok
     assert Storage.get_list == @parsed_list
 
     assert Storage.up_to_date?
     assert Fetcher.update_list(:reboot) == nil
 
     File.rm(@storage_path)
+
+    assert Fetcher.update_list(:reboot) == :ok
+    assert Storage.get_list == @parsed_list
+
+    File.rm(@storage_path)
   end
 
-  test "rate limited: no old data", %{bypass: bypass} do
+  test "rate limited", %{bypass: bypass} do
     Bypass.expect(bypass, "GET", "/",
       &send_resp(&1, 200, @list))
 
-    Bypass.expect(bypass, "GET", @repo1_uri.path,
+    Bypass.expect(bypass, "GET", @repo1_uri,
       &send_resp(&1, 200, @repo1_json))
 
-    Bypass.expect(bypass, "GET", @repo2_uri.path,
+    Bypass.expect(bypass, "GET", @repo2_uri,
       &send_resp(Map.put(&1, :resp_headers, @rate_limit), 403, ""))
 
-    Bypass.expect(bypass, "GET", @repo3_uri.path,
+    Bypass.expect(bypass, "GET", @repo3_uri,
       &send_resp(&1, 200, @repo3_json))
 
-    Bypass.expect(bypass, "GET", @repo4_uri.path,
+    Bypass.expect(bypass, "GET", @repo4_uri,
       &send_resp(&1, 404, ""))
 
     assert Fetcher.update_list(:daily) == :ok
     assert Storage.get_list == @parsed_list_rate_limited
 
     File.rm(@storage_path)
-  end
 
-  test "rate limited: has old data", %{bypass: bypass} do
     Storage.write_list(@parsed_list)
-    Bypass.expect(bypass, "GET", "/",
-      &send_resp(&1, 200, @list))
-
-    Bypass.expect(bypass, "GET", @repo1_uri.path,
-      &send_resp(&1, 200, @repo1_json))
-
-    Bypass.expect(bypass, "GET", @repo2_uri.path,
-      &send_resp(Map.put(&1, :resp_headers, @rate_limit), 403, ""))
-
-    Bypass.expect(bypass, "GET", @repo3_uri.path,
-      &send_resp(&1, 200, @repo3_json))
-
-    Bypass.expect(bypass, "GET", @repo4_uri.path,
-      &send_resp(&1, 404, ""))
 
     assert Fetcher.update_list(:daily) == :ok
     assert Storage.get_list == @parsed_list
